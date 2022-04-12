07c8239bd94be5851ad7807f6ac27ab1d2544d2a
yeongwoo_Cho <jyy0223@naver.com>
2022-04-12 23:17:57 +0900

    스플레쉬 뷰 주석 처리

--- ProductiveThree/ProductiveThree/Loading/SplashView.swift
+++ ProductiveThree/ProductiveThree/Loading/SplashView.swift
@@ -1,47 +1,47 @@
+////
+////  SplashView.swift
+////  ProductiveThree
+////
+////  Created by 김유나 on 2022/04/12.
+////
 //
-//  SplashView.swift
-//  ProductiveThree
+//import SwiftUI
 //
-//  Created by 김유나 on 2022/04/12.
+//private let defaultTimeout: TimeInterval = 2.0
 //
-
-import SwiftUI
-
-private let defaultTimeout: TimeInterval = 2.0
-
-struct SplashView<SplashContent: View>: ViewModifier {
-    private let timeout: TimeInterval
-    private let splashContent: () -> SplashContent
-    
-    @State private var isActive = true
-    
-    init(timeout: TimeInterval = defaultTimeout,
-         @ViewBuilder splashContent: @escaping () -> SplashContent) {
-        self.timeout = timeout
-        self.splashContent = splashContent
-    }
-    
-    func body(content: Content) -> some View {
-        if isActive {
-            splashContent()
-                .onAppear {
-                    DispatchQueue.main.asyncAfter(deadline: .now() + timeout) {
-                        withAnimation {
-                            self.isActive = false
-                        }
-                    }
-                }
-        } else {
-            content
-        }
-    }
-}
-
-extension View {
-    func splashView<SplashContent: View>(
-        timeout: TimeInterval = defaultTimeout,
-        @ViewBuilder splashContent: @escaping () -> SplashContent
-    ) -> some View {
-        self.modifier(SplashView(timeout: timeout, splashContent: splashContent))
-    }
-}
+//struct SplashView<SplashContent: View>: ViewModifier {
+//    private let timeout: TimeInterval
+//    private let splashContent: () -> SplashContent
+//    
+//    @State private var isActive = true
+//    
+//    init(timeout: TimeInterval = defaultTimeout,
+//         @ViewBuilder splashContent: @escaping () -> SplashContent) {
+//        self.timeout = timeout
+//        self.splashContent = splashContent
+//    }
+//    
+//    func body(content: Content) -> some View {
+//        if isActive {
+//            splashContent()
+//                .onAppear {
+//                    DispatchQueue.main.asyncAfter(deadline: .now() + timeout) {
+//                        withAnimation {
+//                            self.isActive = false
+//                        }
+//                    }
+//                }
+//        } else {
+//            content
+//        }
+//    }
+//}
+//
+//extension View {
+//    func splashView<SplashContent: View>(
+//        timeout: TimeInterval = defaultTimeout,
+//        @ViewBuilder splashContent: @escaping () -> SplashContent
+//    ) -> some View {
+//        self.modifier(SplashView(timeout: timeout, splashContent: splashContent))
+//    }
+//}
