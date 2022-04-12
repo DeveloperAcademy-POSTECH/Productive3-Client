ed0b06cbb32da630f0d6f26b9a8ae0e6d1bb79f6
HANS5109 <103012796+HANS5109@users.noreply.github.com>
2022-04-12 21:12:44 +0900

    add EditUserInfoView, InsertUserInfo view

--- ProductiveThree/ProductiveThree/EditUserInfo/EditUserInfoView.swift
+++ ProductiveThree/ProductiveThree/EditUserInfo/EditUserInfoView.swift
@@ -8,8 +8,97 @@
 import SwiftUI
 
 struct EditUserInfoView: View {
+    @State var nickname: String = ""
+    @State var moto: String = ""
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        
+        NavigationView {
+            
+            ZStack {
+                Color(red: 0.984313725490196, green: 0.984313725490196, blue: 0.984313725490196)
+                    .edgesIgnoringSafeArea(.all)
+                
+                
+                VStack(alignment: .leading){
+                    
+                    Spacer()
+                        .frame(height: 60)
+                    
+                    
+                    Text("닉네임")
+                        .font(.system(size: 15))
+                    
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 7)
+                            .fill(Color.white)
+                            .frame(width: 346.0, height: 43.0)
+                            .shadow(color: Color(red: 0.725, green: 0.725, blue: 0.725), radius: 1, x: 0, y: 0)
+                        TextField("닉네임을 입력해주세요",text: $nickname )
+                            .padding(.leading)
+                            .frame(width: 346, height: 43)
+                    } // ZStack
+                    
+                    Text("좌우명")
+                        .font(.system(size: 15))
+                        .padding(.top, 10)
+                    
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 7)
+                            .fill(Color.white)
+                            .frame(width: 346, height: 43)
+                            .shadow(color: Color(red: 0.725, green: 0.725, blue: 0.725), radius: 1, x: 0, y: 0)
+                        TextField("좌우명을 입력해주세요",text: $moto)
+                            .padding(.leading)
+                            .frame(width: 346, height: 43)
+                        
+                    } // ZStack
+                    
+                    ZStack{
+                        Spacer()
+                            .frame(height:35)
+                        
+                    } // ZStack
+                    
+                    
+                    ZStack{
+                        Spacer()
+                            .frame(height: 200)
+                    } // Z
+                    
+                    
+                    
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 7)
+                            .fill(Color(red: 0.6980392156862745, green: 0.7215686274509804, blue: 0.6392156862745098))
+                            .frame(width: 346, height: 43)
+                        
+                        
+                        Text("정보 변경")
+                            .fontWeight(.regular)
+                            .foregroundColor(.white)
+                            .font(.system(size: 17))
+                        
+                        //}
+                    } // ZStack
+                    .onTapGesture {
+                        self.mode.wrappedValue.dismiss()
+                    }
+                } //NavigationLink
+                
+                
+                //self.mode.wrappedValue.dismiss()
+                
+                
+            } // ZStack
+            .navigationTitle("내 정보 수정하기")
+            
+        } // ZStack
+        
+        
     }
 }
 
--- ProductiveThree/ProductiveThree/InsertUserInfo/InsertUserInfoView.swift
+++ ProductiveThree/ProductiveThree/InsertUserInfo/InsertUserInfoView.swift
@@ -8,8 +8,120 @@
 import SwiftUI
 
 struct InsertUserInfoView: View {
+    @State var nickname: String = ""
+    @State var moto: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            
+            ZStack {
+                Color(red: 0.984313725490196, green: 0.984313725490196, blue: 0.984313725490196)
+                    .edgesIgnoringSafeArea(.all) //background
+                
+                
+                VStack(alignment: .leading){
+                    Text("내 정보 입력하기")
+                        .font(.system(size: 30))
+                        .fontWeight(.bold)
+                    Spacer()
+                        .frame(height: 60)
+                    
+                    
+                    Text("닉네임")
+                        .font(.system(size: 15))
+                    
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 7)
+                            .fill(Color.white)
+                            .frame(width: 346.0, height: 43.0)
+                            .shadow(color: Color(red: 0.725, green: 0.725, blue: 0.725), radius: 1, x: 0, y: 0)
+                        TextField("닉네임을 입력해주세요",text: $nickname )
+                            .padding(.leading)
+                            .frame(width: 346, height: 43)
+                        Spacer()
+                            .frame(height: 24)
+                    } // ZStack
+                    
+                    Text("좌우명")
+                        .font(.system(size: 15))
+                        .padding(.top, 10)
+                    
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 7)
+                            .fill(Color.white)
+                            .frame(width: 346, height: 43)
+                            .shadow(color: Color(red: 0.725, green: 0.725, blue: 0.725), radius: 1, x: 0, y: 0)
+                        TextField("좌우명을 입력해주세요",text: $moto)
+                            .padding(.leading)
+                            .frame(width: 346, height: 43)
+                        
+                    } // ZStack
+                    
+                    ZStack{
+                        Spacer()
+                            .frame(height:35)
+                        
+                        
+                        
+                        HStack{
+                            Text("생산적인3의 서비스 이용약관을 확인해주세요")
+                                .font(.system(size: 12))
+                                .fontWeight(.regular)
+                            
+                            Spacer()
+                            NavigationLink(destination: TermsOfService()){
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 7)
+                                        .fill(Color(red: 0.5725490196078431, green: 0.7686274509803922, blue: 0.803921568627451))
+                                        .frame(width: 70, height: 16)
+                                    // Button(action:{
+                                    // print("이용약관")
+                                    // }) {
+                                    Text("이용약관")
+                                        .fontWeight(.regular)
+                                        .foregroundColor(Color(hue: 1.0, saturation: 0.013, brightness: 1.0))
+                                        .font(.system(size: 12))
+                                    // }
+                                    
+                                }
+                            }
+                        }
+                        .frame(width: 345.68)
+                        
+                    } // ZStack
+                    
+                    
+                    ZStack{
+                        Spacer()
+                            .frame(height: 320)
+                    } // Z
+                    NavigationLink(destination: test2()){
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 7)
+                                .fill(Color(red: 0.6980392156862745, green: 0.7215686274509804, blue: 0.6392156862745098))
+                                .frame(width: 346, height: 43)
+                            
+                            //                      Button(action:{
+                            //                            print("동의 후 회원가입 하기 button tapped!")
+                            //                        }) {
+                            Text("동의 후 회원가입 하기")
+                                .fontWeight(.regular)
+                                .foregroundColor(.white)
+                                .font(.system(size: 17))
+                            
+                            //}
+                        } // ZStack
+                        
+                    }
+                    
+                    
+                    
+                    
+                    
+                } // VStack
+                
+            } // ZStack
+            .navigationBarHidden(true)
+        } // navigationview
     }
 }
 
