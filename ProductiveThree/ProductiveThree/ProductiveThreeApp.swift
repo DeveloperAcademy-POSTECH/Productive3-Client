9a6a4bbfa001a6136daddea34a53f4d4db119a50
yeongwoo_Cho <jyy0223@naver.com>
2022-04-12 15:59:47 +0900

    project init

---
+++ ProductiveThree/ProductiveThree/ProductiveThreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductiveThreeApp.swift
+//  ProductiveThree
+//
+//  Created by yeongwoocho on 2022/04/12.
+//
+
+import SwiftUI
+
+@main
+struct ProductiveThreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
