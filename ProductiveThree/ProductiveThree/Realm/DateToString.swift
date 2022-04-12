f54bedae3958b05553f67d025f3937fe77f33059
LeoBang <bluke8489@gmail.com>
2022-04-12 21:13:32 +0900

    add Models and datetostring static class

---
+++ ProductiveThree/ProductiveThree/Realm/DateToString.swift
@@ -0,0 +1,16 @@
+//
+//  DateToString.swift
+//  ProductiveThree
+//
+//  Created by Leo Bang on 2022/04/12.
+//
+
+import Foundation
+
+class DateToString {
+    static func resolve(date: Date) -> String {
+        let formatter = DateFormatter();
+        formatter.dateFormat = "yyyy-MM-dd";
+        return formatter.string(from: date)
+    }
+}
