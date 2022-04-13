dda0d6456011168ba7decebc5682152c623700b1
Taeho Kim <xogh8025@gmail.com>
2022-04-13 16:19:02 +0900

    add: screen switch system

---
+++ ProductiveThree/ProductiveThree/AskUserInfo/AskUserInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  AskUserInfoView.swift
+//  ProductiveThree
+//
+//  Created by 김태호 on 2022/04/13.
+//
+
+import SwiftUI
+
+struct AskUserInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AskUserInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        AskUserInfoView()
+    }
+}
