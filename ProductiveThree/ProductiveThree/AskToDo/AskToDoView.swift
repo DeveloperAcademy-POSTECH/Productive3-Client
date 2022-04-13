4fe6c7ee37d72ab8054c1619c6d3b0f4dfbeef16
Taeho Kim <xogh8025@gmail.com>
2022-04-13 21:59:35 +0900

    add screen swtich system

---
+++ ProductiveThree/ProductiveThree/AskToDo/AskToDoView.swift
@@ -0,0 +1,20 @@
+//
+//  AskToDoView.swift
+//  ProductiveThree
+//
+//  Created by 김태호 on 2022/04/13.
+//
+
+import SwiftUI
+
+struct AskToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AskToDoView_Previews: PreviewProvider {
+    static var previews: some View {
+        AskToDoView()
+    }
+}
---
+++ ProductiveThree/ProductiveThree/ScreenSwtich/ScreenSwtichView.swift
@@ -0,0 +1,32 @@
+//
+//  ScreenSwtichView.swift
+//  ProductiveThree
+//
+//  Created by 김태호 on 2022/04/13.
+//
+
+import SwiftUI
+
+struct ScreenSwtichView: View {
+    @State var viewState = "AskUserInfo"
+    var body: some View {
+        switch viewState {
+        case "InsertUserInfo":
+            InsertUserInfoView()
+        case "AskToDoView":
+            AskToDoView()
+        case "InsertToDo":
+            InsertTodoView()
+//        case "ToDoList":
+//            TodoListView()
+        default :
+            AskUserInfoView()
+        }
+    }
+}
+
+struct ScreenSwtichView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScreenSwtichView()
+    }
+}
