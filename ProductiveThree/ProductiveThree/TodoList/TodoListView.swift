4065431921688eca14a2918de1e3c5f31ef34741
LeoBang <bluke8489@gmail.com>
2022-04-13 16:48:16 +0900

    setting

--- ProductiveThree/ProductiveThree/TodoList/TodoListView.swift
+++ ProductiveThree/ProductiveThree/TodoList/TodoListView.swift
@@ -9,12 +9,21 @@ import SwiftUI
 
 struct TodoListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct TodoListView_Previews: PreviewProvider {
-    static var previews: some View {
-        TodoListView()
+        NavigationView {
+            ZStack {
+                Color.WHITEGRAY_8.edgesIgnoringSafeArea(.all)
+                //
+                
+                
+            }
+            .navigationBarItems(
+                trailing:
+                    NavigationLink(destination: MyPageView()) {
+                        Image("person.fill")
+                            .renderingMode(.template)
+                            .foregroundColor(Color.OLIVE_8)
+                    }
+            )
+        }
     }
 }
